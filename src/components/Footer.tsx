60b69a7027a0bf4b0b35ec6c516ec82ed8ce39e6
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2025-11-27 21:43:35 -0300

    feat: add Footer component to Home page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <div>
+      <footer className="bg-[#213559] text-white text-center p-4 mt-8">
+        <p>
+          &copy; 2025 Alan Rodriges, Fabricio Fontenele, Francisco Alves, Ruan
+          Pedro
+        </p>
+      </footer>
+    </div>
+  );
+};
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,10 @@
+import { Footer } from "../components/Footer";
 import { HeroSection } from "../components/HeroSection";
 export const Home = () => {
   return (
     <div className="min-h-screen bg-[#213559]">
       <HeroSection />
+      <Footer />
     </div>
   );
 };
