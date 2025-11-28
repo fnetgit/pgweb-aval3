19599f919e918b3722433d77b6bd6f7c06139646
Ruan Anjos <anjosruanp@gmail.com>
2025-11-28 10:08:01 -0300

    fix: update image and heading styles in HeroSection for improved responsiveness

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,9 +9,9 @@ export const HeroSection = ({ onSearch, onFilter }: HeroSectionProps) => {
   return (
     <div className="flex items-center justify-center h-[60vh] sm:h-[70vh] md:h-screen">
       <div className="relative w-full px-4">
-        <img src="/theEarth.png" alt="Earth" className="w-full max-w-md mx-auto" />
-        <div className="absolute inset-0 flex items-center justify-center flex-col gap-3 sm:gap-4 -mt-40 sm:-mt-48 md:-mt-60">
-          <h1 className="text-white text-2xl sm:text-3xl md:text-4xl">
+        <img src="/theEarth.png" alt="Earth" className="w-full max-w-md md:max-w-4xl lg:max-w-4x1 mx-auto" />
+        <div className="absolute inset-0 flex items-center justify-center flex-col gap-3 sm:gap-4 -mt-12 sm:-mt-16 md:-mt-20">
+          <h1 className="text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
             <span className="font-bold">Geo</span>Pedia
           </h1>
           <SearchInput onChange={onSearch} onFilterChange={onFilter} />
